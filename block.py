29d1af36a076466d78a02af4d7b2d4b5c6b56169
UKSubikshaa <113699761+UKSubikshaa@users.noreply.github.com>
2026-03-13 15:38:11 +0530

    Update block.py

--- block.py
+++ block.py
@@ -2,11 +2,21 @@ import pickle
 import numpy as np
 
 # Load the trained model
-with open("model.pkl", "rb") as f:
+with open(import pickle
+import numpy as np
+
+with open("edge_ai_model.pkl", "rb") as f:
     model = pickle.load(f)
 
 def process(input, config):
 
+    data = np.array(input)
+
+    prediction = model.predict([data])
+
+    return {
+        "features": prediction.tolist()
+    }
     # Convert incoming data to numpy array
     data = np.array(input)
 
