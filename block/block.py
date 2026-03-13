700403e78f4f74aca0da098d42e2c48800461b29
UKSubikshaa <113699761+UKSubikshaa@users.noreply.github.com>
2026-03-13 15:52:33 +0530

    Update block.py

--- block/block.py
+++ block/block.py
@@ -1,20 +1,15 @@
 import pickle
 import numpy as np
 
-# Load the trained model
 with open("block/edge_ai_model.pkl", "rb") as f:
     model = pickle.load(f)
 
-
 def process(input, config):
 
-    # Convert input data to numpy
     data = np.array(input)
 
-    # Run model prediction
     prediction = model.predict([data])
 
-    # Return result to Edge Impulse
     return {
         "features": prediction.tolist()
     }
